3e177cb785cff819e315ad67b3cc62b037deab46
Magak Emmanuel <emashmagak@gmail.com>
2022-07-27 22:57:09 +0300

    added modal

--- src/components/calendar/calendarHeader.vue
+++ src/components/calendar/calendarHeader.vue
@@ -22,6 +22,7 @@
             <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
           </svg>
         </button>
+        <create-reminder-modal />
       </div>
     </div>
   </header>
@@ -29,6 +30,7 @@
 
 <script setup>
 import dayjs from 'dayjs';
+import CreateReminderModal from '../reminder/createReminderModal.vue';
 
 const props = defineProps({
    selectedDate: {
